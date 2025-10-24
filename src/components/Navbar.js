d90cb2daa85589b450f2558c025ae2d621a873b1
Lakshmi <lakshmisowparnikam@gmail.com>
2025-10-24 22:09:33 +0530

    Create Navbar.js

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import { Link } from 'react-router-dom';
+
+export default function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">Employee Dashboard</Link>
+        <div className="collapse navbar-collapse">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item"><Link className="nav-link" to="/">Home</Link></li>
+            <li className="nav-item"><Link className="nav-link" to="/employee">Employee Form</Link></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
